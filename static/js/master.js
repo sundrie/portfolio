6b582a55ae3a0039be22e8fafe2d87edb4ed2fc5
sundrie <sundrie42@gmail.com>
2017-02-02 19:50:55 +0100

    made scroll animation with anchor

--- static/js/master.js
+++ static/js/master.js
@@ -1,12 +1,19 @@
 $(function(){
 
-  function scrollToAnchor(aid){
-      var anchorTarget = $("#"+ aid +"");
-      $('html,body').animate({scrollTop: anchorTarget.offset().top},'slow');
+// On sélectionne tous les liens contenant # (ancres)
+
+  $('a[href*="#"]').click(function(){
+    // On récupère la cible du lien de l'ancre
+    var href = $(this).attr('href');
+    // On retire le # inutile
+    var validhref = href.replace('#', '');
+    scrollToAnchor(validhref);
+  });
+
+  function scrollToAnchor(ancre){
+    var target = $("div [id="+ ancre +"]");
+    $('html,body').animate({scrollTop: target.offset().top},'slow');
   }
 
-  $("#contact").click(function() {
-     scrollToAnchor('contact');
-  });
 
 });
